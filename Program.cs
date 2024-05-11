48336b4c5748365517986a3cea6f657f9f650ca5
oleg <draconfantom@gmail.com>
2024-05-11 23:44:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLessons1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sectors = { 6, 28, 15, 15, 17 };
+            bool isOpen = true;
+           
+            while (isOpen)
+            {
+                Console.SetCursorPosition(0,18);
+                for (int i = 0; i < sectors.Length; i++)
+                {
+                    Console.WriteLine($"В секторе {i+1} Свободно {sectors[i]} Мест.");
+
+                }
+                Console.SetCursorPosition(0,0);
+                Console.WriteLine("Регистрация рейса");
+                Console.WriteLine("\n\n1 - Забронировать место\n\n2 - Выход из програмы\n\n");
+                Console.Write("Введите номер команды: ");
+
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        int userSector, userPlaseAmount;
+                        Console.Write("В каком секторе вы хотите лететь? ");
+                        userSector = Convert.ToInt32(Console.ReadLine())-1;
+                        if (sectors.Length <= userSector || userSector < 0) 
+                        {
+                            Console.WriteLine("Такого сектора не сушествует.");
+                            break;
+                        }
+                        Console.Write("Сколько мест вы хотите забронировать? ");
+                        userPlaseAmount = Convert.ToInt32(Console.ReadLine());
+                        if (userPlaseAmount<0)
+                        {
+                            Console.WriteLine("Неверное количество мест.");
+                            break;
+                        }
+                        if (sectors[userSector] < userPlaseAmount || userPlaseAmount<0)
+                        {
+                            Console.WriteLine($"В секторе :{userSector+1}: недостотаточно мест." +
+                                $" Остаток {sectors[userSector]}" ) ;
+                            break;
+                        }
+                        sectors[userSector] -= userPlaseAmount;
+                        break;
+                    case 2:
+                        isOpen = false;
+                        break;
+                    default:
+                        break;
+                }
+
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("NewLessons1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NewLessons1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("43183a0a-f766-4671-82e9-29cb60cd3a09")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
